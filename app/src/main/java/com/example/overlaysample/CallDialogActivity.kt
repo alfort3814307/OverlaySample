0f5995f64ecff534ded0356c683fba72f392dc18
alfort3814307 <alfort3814307@gmail.com>
2023-05-14 21:23:43 +0900

    メッセージ用Activityを表示できるようにした

--- app/src/main/java/com/example/overlaysample/CallDialogActivity.kt
+++ app/src/main/java/com/example/overlaysample/CallDialogActivity.kt
@@ -1,10 +1,10 @@
 package com.example.overlaysample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 
-class CallDialogActivity : AppCompatActivity() {
+class CallDialogActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_dialog)
